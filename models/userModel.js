c47befb9a65a7fa17cf66b05c0e5e8d02eb5e3bf
xart22 <fajar.rn22@gmail.com>
2021-02-04 21:19:57 +0700

    Adding login & logout

--- models/userModel.js
+++ models/userModel.js
@@ -1,17 +1,15 @@
-const mongoose = require("mongoose")
+const { model, Schema } = require("mongoose");
 
 
 
-const userShcema = new mongoose.Schema({
-    username : {type:String},
-    email : {type:String,required:true},
-    passwordHas : {type:String,required:true},
-    phone:{type:String},
-    alamat:{type:String},
-    avatar:{type:String},
-    favorit:{type:String},
+const userShcema = new Schema({
+    email : String,
+    passwordHash : String,
+    username :String ,
+    phone:String,
+    alamat:String,
+    avatar:String,
+    favorit:String,
 })
 
-const User = mongoose.model("user", userShcema)
-
-module.exports= User;
+module.exports= model("User",userShcema);
